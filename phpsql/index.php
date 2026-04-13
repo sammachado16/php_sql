39d9aff8141f0fe29f4000b05e40edc9aca700b6
Samuel Machado <sam.machado16@gmail.com>
2026-04-13 12:54:56 -0300

    Início de Index e CSS

---
+++ phpsql/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<title>Listagem de Jogos</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="estilo/estilo.css"/>
+</head>
+<body>
+	<div id="corpo">
+		<h1>Escolha seu jogo</h1>	
+		<table class="listagem"/>
+			<tr><td>Foto<td>Nome<td>Admin
+			<tr><td>Foto<td>Nome<td>Admin
+			<tr><td>Foto<td>Nome<td>Admin
+			<tr><td>Foto<td>Nome<td>Admin
+		</table>
+	</div>
+</body>
+</html>
---
+++ phpsql/modelo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<title>Título da Página</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="estilo/estilo.css"/>
+</head>
+<body>
+	<div id="corpo">
+		<h1>Olá!</h1>
+	</div>
+</body>
+</html>
