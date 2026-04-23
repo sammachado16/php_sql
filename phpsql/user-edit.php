1d3dd28e59af17d251441dd0c64e19d05de17d77
Samuel Machado <sam.machado16@gmail.com>
2026-04-22 21:21:27 -0300

    Alteração completa

--- phpsql/user-edit.php
+++ phpsql/user-edit.php
@@ -20,7 +20,34 @@
                 if(!isset($_POST['usuario'])) {
                     include "user-edit-form.php";
                 } else {
-                    echo msg_sucesso("Sucesso");
+                    $usuario = $_POST['usuario'] ?? null;
+                    $nome = $_POST['nome'] ?? null;
+                    $senha1 = $_POST['senha1'] ?? null;
+                    $senha2 = $_POST['senha2'] ?? null;
+                    $tipo = $_POST['tipo'] ?? null;
+
+                    $q = "update usuarios set usuario = '$usuario', nome = '$nome'";
+
+                    if (empty($senha1) || is_null($senha1)) {
+                        echo msg_aviso("Senha antiga foi mantida.");
+                    } else {
+                        if ($senha1 === $senha2) {
+                            $senha = gerarHash($senha1);
+                            $q .= ", senha='$senha'";
+                        } else {
+                            echo msg_erro("Senhas não conferem. A senha anterior será mantida");
+                        }
+                    }
+
+                    $q .= " where usuario= '" . $_SESSION['user']. "'";
+
+                    if ($banco->query($q)) {
+                        echo msg_sucesso("Usuário alterado com sucesso!");
+                        logout();
+                        echo msg_aviso("Por segurança, efetue <a href='user-login.php'>login</a> novamente.");
+                    } else {
+                        echo msg_erro("Não foi possível alterar os dados");
+                    }
                 }
             }
         ?>
